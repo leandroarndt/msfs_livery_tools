77d42676b81f9eeda3f57625ab4f1969b41cd495
leandroarndt <poetalirico@gmail.com>
2023-05-01 00:48:31 -0300

    Layout generator corrected
    Now it ignores layout.json and manifest.json

--- msfs_livery_tools/package/layout.py
+++ msfs_livery_tools/package/layout.py
@@ -12,10 +12,11 @@ def create_layout(package_dir:str):
     layout = {'content': []}
     for file in Path(package_dir).glob('**/*'):
         if file.is_file():
-            layout['content'].append({
-                'path': file.relative_to(package_dir).as_posix(),
-                'size': Path(file).stat().st_size,
-                'date': win_time(Path(file).stat().st_mtime_ns),
-            })
+            if not (file.name.lower() == 'layout.json' or file.name.lower() == 'manifest.json'):
+                layout['content'].append({
+                    'path': file.relative_to(package_dir).as_posix(),
+                    'size': Path(file).stat().st_size,
+                    'date': win_time(Path(file).stat().st_mtime_ns),
+                })
     with Path(package_dir, 'layout.json').open('w') as f:
         json.dump(layout, f, indent=4)
