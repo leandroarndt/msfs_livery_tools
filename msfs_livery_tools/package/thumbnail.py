645497d39abed7dcb970d6619b05e38a34b49f74
leandroarndt <poetalirico@gmail.com>
2023-05-05 19:33:46 -0300

    Resize to both full-size and small thumbnails

--- msfs_livery_tools/package/thumbnail.py
+++ msfs_livery_tools/package/thumbnail.py
@@ -7,22 +7,36 @@ import __main__
 def placeholder(path:str|Path):
     shutil.copy(__main__.RESOURCES_DIR / 'thumbnail.jpg', path)
 
+def gap(image:Image, size:tuple[int])->dict:
+    ratio = max(size[0] / image.size[0], size[1] / image.size[1])
+    return {
+        'xmin': int(((image.size[0] * ratio - size[0]) / 2) / ratio),
+        'xmax': int((image.size[0] * ratio - (image.size[0] * ratio - size[0]) / 2) / ratio),
+        'ymin': int(((image.size[1] * ratio - size[1]) / 2) / ratio),
+        'ymax': int((image.size[1] * ratio - (image.size[1] * ratio - size[1]) / 2) / ratio),
+    }
+
 def resize_thumbnail(path:str|Path):
+    # https://docs.flightsimulator.com/html/Samples_And_Tutorials/Tutorials/Creating_A_Livery_Package.ht
     path = Path(path)
-    size = (600, 216)
-    if Path(path.parent, f'{path.stem}.png').exists(): # Converts in-game capture
+    size_big = (1618, 582)
+    size_small = (600, 216)
+    
+    # Search for in-game capture
+    if Path(path.parent, f'{path.stem}.png').exists():
         path = Path(path.parent, f'{path.stem}.png')
         thumbnail = Image.open(path)
         thumbnail.save(Path(path.parent, f'{path.stem}.jpg'))
         path.unlink() # Prevents converting to DDS
     else:
         thumbnail = Image.open(path)
-    ratio = max(size[0] / thumbnail.size[0], size[1] / thumbnail.size[1])
-    gap = {
-        'xmin': int(((thumbnail.size[0] * ratio - size[0]) / 2) / ratio),
-        'xmax': int((thumbnail.size[0] * ratio - (thumbnail.size[0] * ratio - size[0]) / 2) / ratio),
-        'ymin': int(((thumbnail.size[1] * ratio - size[1]) / 2) / ratio),
-        'ymax': int((thumbnail.size[1] * ratio - (thumbnail.size[1] * ratio - size[1]) / 2) / ratio),
-    }
-    resized = thumbnail.resize((600, 216), box=(gap['xmin'], gap['ymin'], gap['xmax'], gap['ymax']))
+        
+    # Resizes to small
+    resize_gap = gap(thumbnail, size_small)
+    resized = thumbnail.resize(size_small, box=(resize_gap['xmin'], resize_gap['ymin'], resize_gap['xmax'], resize_gap['ymax']))
     resized.save(Path(path).parent / 'thumbnail-small.jpg')
+
+    # Resizes to big
+    resize_gap = gap(thumbnail, size_big)
+    resized = thumbnail.resize(size_big, box=(resize_gap['xmin'], resize_gap['ymin'], resize_gap['xmax'], resize_gap['ymax']))
+    resized.save(Path(path).parent / 'thumbnail.jpg')
