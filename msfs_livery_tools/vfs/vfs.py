fad156832e9796b4bf2acc18563c4f67282ee69c
leandroarndt <poetalirico@gmail.com>
2023-05-06 18:58:42 -0300

    Scans packages

--- msfs_livery_tools/vfs/vfs.py
+++ msfs_livery_tools/vfs/vfs.py
@@ -5,15 +5,17 @@ from pathlib import Path
 class _VFSObject(object):
     name:str
     parent:object
+    base_path:Path
     
-    def __init__(self, name:str, parent, *args, **kwargs):
+    def __init__(self, name:str, parent, base_path, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = name.lower()
         self.parent = parent
-        self.parent.contents.add(self)
+        self.parent.contents[self.name] = self
+        self.base_path = base_path
 
 class VFSFolder(_VFSObject):
-    contents:set[_VFSObject]
+    contents:dict = {}
     
     def __hash__(self):
         return id(self)
@@ -28,22 +30,44 @@ class VFSFolder(_VFSObject):
         return self.folder != other.folder
     
     def __contains__(self, name:str):
-        for item in self.contents:
-            if item.name == name:
-                return(True)
-        return False
+        return name in self.contents
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.contents = set()
+        self.contents = {}
     
     @classmethod
-    def new(cls, name, parent, layout:Path|None=None, *args, **kwargs):
-        if name in parent:
-            for content in parent.contents:
-                if content.name == name:
-                    return content
-        return VFSFolder(name, parent)
+    def new(cls, name:str, parent, base_path:Path, layout:Path|None=None, *args, **kwargs):
+        if name.lower() in parent:
+            instance = parent.contents[name.lower()]
+        else:
+            instance = VFSFolder(name, parent, base_path, *args, **kwargs)
+        
+        if isinstance(parent, VFS):
+            instance.root = parent
+        else:
+            instance.root = parent.root
+            
+        if layout:
+            instance.scan_layout(layout)
+        
+        return instance
+
+    @classmethod
+    def scan_layout(cls, layout_file:Path, root):
+        with layout_file.open('r') as f:
+            layout = json.load(f)
+        for item in layout['content']:
+            try:
+                path = Path(item['path'])
+                part_parent = root
+                folder = VFSFolder # Avoid UnboundLocalError
+                for part in path.parent.parts:
+                    folder = VFSFolder.new(part, part_parent, base_path=layout_file.parent)
+                    part_parent = folder
+                VFSFile(path, folder, layout_file.parent)
+            except KeyError:
+                pass
     
     def find(file_name:str, paths:list[str])->Path:
         """Finds a file "file_name" in "paths" and returns the corresponding real file system path.
@@ -60,15 +84,15 @@ class VFSFolder(_VFSObject):
 class VFSFile(_VFSObject):
     file:Path
     
-    def __init__(self, file:str|Path, parent:VFSFolder, *args, **kwargs):
+    def __init__(self, file:str|Path, parent:VFSFolder, base_path:Path, *args, **kwargs):
         self.file = Path(file)
-        super().__init__(name=self.file.name, parent=parent, *args, **kwargs)
+        super().__init__(name=self.file.name, parent=parent, base_path=base_path, *args, **kwargs)
     
     def open(self, mode:str, *args, **kwargs):
-        return self.file.open(mode, *args, **kwargs)
+        return (self.base_path / self.file).open(mode, *args, **kwargs)
 
 class VFS(object):
-    contents:set[VFSFolder]
+    contents:dict = {}
     package_folder:Path
     _instance = None
     
@@ -78,7 +102,7 @@ class VFS(object):
         if cls._instance is None:
             cls._instance = cls()
             cls._instance.package_folder = Path(package_folder)
-            cls._instance.contents = set()
+            cls._instance.contents = {}
             cls._instance.scan(include_all)
         
         return cls._instance
@@ -93,10 +117,7 @@ class VFS(object):
         return self.package_folder != other.package_folder
     
     def __contains__(self, name:str):
-        for item in self.contents:
-            if item.name == name:
-                return(True)
-        return False
+        return name in self.contents
     
     def scan(self, include_all:bool=False):
         if include_all:
@@ -105,4 +126,4 @@ class VFS(object):
             packages = list((self.package_folder / 'Official').glob('**/layout.json')) + list((self.package_folder / 'Community').glob('**/layout.json'))
         for package in packages:
             print(f'Adding {package.parent.name} into VFS root.')
-            VFSFolder.new(package.parent.name, self, package)
+            VFSFolder.scan_layout(package, self)
