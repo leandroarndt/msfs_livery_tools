eccb66b79e248c2a282db33f55238c1d91997f17
leandroarndt <poetalirico@gmail.com>
2023-05-07 13:50:40 -0300

    pathlib

--- msfs_livery_tools/compression/dds.py
+++ msfs_livery_tools/compression/dds.py
@@ -1,7 +1,8 @@
 """Deals with DDS compression using Microsoft's texconv (https://github.com/Microsoft/DirectXTex/wiki/Texconv)."""
-import os, subprocess, json
+import subprocess, json
+from pathlib import Path
 
-def convert(input:str, output_dir:str, texconv_path:str, out_type:str='dds', out_format:str='',
+def convert(input:str|Path, output_dir:str|Path, texconv_path:str|Path, out_type:str='dds', out_format:str='',
             overwrite:bool=True):
     """Converts image types."""
     command = [texconv_path, '-o', output_dir, '-ft', out_type,]
@@ -13,21 +14,17 @@ def convert(input:str, output_dir:str, texconv_path:str, out_type:str='dds', out
     proc = subprocess.run(command)
     return proc.returncode
 
-def from_glft(input_gltf:str, input_dir:str, output_dir:str, texconv_path:str, type:str='png'):
+def from_glft(input_gltf:str|Path, input_dir:str|Path, output_dir:str|Path, texconv_path:str|Path, out_format:str='png'):
     """Converts glTF DDS textures into another format."""
     # Open glTF
-    f = open(input_gltf)
-    gltf = json.load(f)
-    f.close()
+    with Path(input_gltf).open('r') as f:
+        gltf = json.load(f)
     
     # Uncompress
     for image in gltf['images']:
         try:
             # uncompresses DDS into PNG
-            convert(
-                    os.path.join(input_dir, image['uri']),
-                    output_dir, texconv_path, type
-            )
+            convert(Path(input_dir) / image['uri'], output_dir, texconv_path, out_format)
             
         except KeyError: # not compressed, continue working!
             pass
