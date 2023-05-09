ad9208879a13e6cd1abbbc5b52a5ff62594d350e
leandroarndt <poetalirico@gmail.com>
2023-05-08 21:14:30 -0300

    Extract texture from fallback directories

--- msfs_livery_tools/compression/dds.py
+++ msfs_livery_tools/compression/dds.py
@@ -1,6 +1,7 @@
 """Deals with DDS compression using Microsoft's texconv (https://github.com/Microsoft/DirectXTex/wiki/Texconv)."""
 import subprocess, json
 from pathlib import Path
+from msfs_livery_tools.vfs import VFSFolder
 
 def convert(input:str|Path, output_dir:str|Path, texconv_path:str|Path, out_type:str='dds', out_format:str='',
             overwrite:bool=True):
@@ -14,7 +15,8 @@ def convert(input:str|Path, output_dir:str|Path, texconv_path:str|Path, out_type
     proc = subprocess.run(command)
     return proc.returncode
 
-def from_glft(input_gltf:str|Path, input_dir:str|Path, output_dir:str|Path, texconv_path:str|Path, out_format:str='png'):
+def from_glft(input_gltf:str|Path, input_dir:str|Path|VFSFolder, output_dir:str|Path, texconv_path:str|Path,
+                out_format:str='png', fallbacks:list[str]=[]):
     """Converts glTF DDS textures into another format."""
     # Open glTF
     with Path(input_gltf).open('r') as f:
@@ -23,8 +25,14 @@ def from_glft(input_gltf:str|Path, input_dir:str|Path, output_dir:str|Path, texc
     # Uncompress
     for image in gltf['images']:
         try:
-            # uncompresses DDS into PNG
-            convert(Path(input_dir) / image['uri'], output_dir, texconv_path, out_format)
+            if isinstance(input_dir, str) or isinstance(input_dir, Path):
+                convert(Path(input_dir) / image['uri'], output_dir, texconv_path, out_format)
+            else:
+                try:
+                    convert(input_dir.find(image['uri'], fallbacks=fallbacks).real_path(),
+                            output_dir, texconv_path, out_format)
+                except FileNotFoundError:
+                    pass
             
         except KeyError: # not compressed, continue working!
             pass
