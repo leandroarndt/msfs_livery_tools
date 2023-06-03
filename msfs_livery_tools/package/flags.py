71069a76bc83132552f2c30438db25a05f092f30
leandroarndt <poetalirico@gmail.com>
2023-06-03 13:10:15 -0300

    flags set directly accessible

--- msfs_livery_tools/package/flags.py
+++ msfs_livery_tools/package/flags.py
@@ -8,18 +8,18 @@ class Flags (object):
     ALPHA_PRESERVATION = 'ALPHAPRESERVATION'
     
     file:Path
-    _flags:set
+    flags:set
     
     def __init__(self, flags:list, file:str|Path|None=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if file:
             self.file = Path(file)
-        self._flags = set()
+        self.flags = set()
         for flag in flags:
-            self._flags.add(flag)
+            self.flags.add(flag)
     
     def __str__(self)->str:
-        return f'_DEFAULT=+{"+".join(self._flags)}'
+        return f'_DEFAULT=+{"+".join(self.flags)}'
     
     @classmethod
     def open(cls, file:str|Path):
@@ -36,10 +36,3 @@ class Flags (object):
             raise ValueError('Cannot save without file path.')
         with self.file.open('w', encoding='utf-8') as f:
             f.write(str(self))
-    
-    def add(self, flag:str):
-        self._flags.add(flag)
-    
-    def remove(self, flag:str):
-        self._flags.discard(flag)
-
